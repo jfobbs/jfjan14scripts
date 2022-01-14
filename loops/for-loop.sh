eaf8cc7b97bc47f19707055daef23f2e630dc635
Jo <jfobbs@ucsb.edu>
2022-01-14 11:06:35 -0800

    ignore data files and results dir

--- loops/for-loop.sh
+++ loops/for-loop.sh
@@ -1,1 +1,11 @@
-comment
+#for-loop example
+cards= (1,2,2,4,5,6,7,8,9,10,10,10)
+
+echo "how many cards would you like?"
+read value
+
+for i in $(seq 1 $value);
+do echo $(cards[RANDOM%${#cards[@]}]};
+done
+
+
